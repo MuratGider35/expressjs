ad5f09cc713126230cc42617b431c91de6bfcc2c
MuratGider35 <m.gider35@gmail.com>
2023-10-04 22:55:11 +0300

    create

---
+++ BlogAPI_Mongoose/src/controllers/blogController.js
@@ -0,0 +1,46 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+// https://mongoosejs.com/docs/queries.html
+
+// Catch async-errors and send to errorHandler:
+require("express-async-errors");
+
+// Call Models:
+const { BlogPost } = require("../models/blogModel");
+
+// ------------------------------------------
+// BlogPost
+// ------------------------------------------
+module.exports.BlogPost = {
+  list: async (req, res) => {
+    const data = await BlogPost.find();
+
+    res.status(200).send({
+      error: false,
+      result: data,
+    });
+  },
+
+  create: async (req, res) => {
+    // const data = await BlogPost.insertOne({
+    //     fieldName: 'value',
+    //     fieldName: 'value',
+    //     fieldName: 'value',
+    // })
+    const data = await BlogPost.insertOne(req.body);
+
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      result: data,
+    });
+  },
+
+  read: async (req, res) => {},
+
+  update: async (req, res) => {},
+
+  delete: async (req, res) => {},
+};
